0fa0096a9f5f8e94c117078d6b9a1a2551ed1baa
Tia Giron <tiagiron01@gmail.com>
2025-10-30 16:57:42 -0500

    product list added to schema and local image storage defined in keystone

--- keystone.ts
+++ keystone.ts
@@ -5,27 +5,46 @@
 // Keystone imports the default export of this file, expecting a Keystone configuration object
 //   you can find out more at https://keystonejs.com/docs/apis/config
 
-import { config } from '@keystone-6/core'
+import { config } from "@keystone-6/core";
 
 // to keep this file tidy, we define our schema in a different file
-import { lists } from './schema'
+import { lists } from "./schema";
 
 // authentication is configured separately here too, but you might move this elsewhere
 // when you write your list-level access control functions, as they typically rely on session data
-import { withAuth, session } from './auth'
+import { withAuth, session } from "./auth";
 
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL)
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+const baseUrl = "http://localhost:3000";
 
 export default withAuth(
   config({
     db: {
-      provider: 'postgresql',
-      url: process.env.DATABASE_URL || 'DATABASE_URL_TO_REPLACE',
+      provider: "postgresql",
+      url: process.env.DATABASE_URL || "DATABASE_URL_TO_REPLACE",
     },
     lists,
     session,
-  })
-)
+    storage: {
+      local_image_storage: {
+        // Images that use this store will be stored on the local machine
+        kind: "local",
+        // This store is used for the image field type
+        type: "image",
+        // The URL that is returned in the Keystone GraphQL API
+        generateUrl: (path) => `${baseUrl}/images${path}`,
+        // The route that will be created in Keystone's backend to serve the images
+        serverRoute: {
+          path: "/images",
+        },
+        // Set serverRoute to null if you don't want a route to be created in Keystone
+        // serverRoute: null
+        storagePath: "public/images",
+      },
+    },
+  }),
+);
